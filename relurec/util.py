1b8e67a51f01bf3e855f8854f414c223358fba20
James Kirk <james.f.kirk@gmail.com>
2017-03-01 12:55:42 -0500

    ENH: Adds fetch_movielens from lightfm

--- relurec/util.py
+++ relurec/util.py
@@ -3,6 +3,8 @@ import numpy as np
 import random
 import scipy.sparse as sp
 
+from lightfm.datasets.movielens import fetch_movielens
+
 
 def generate_sparse_input(interactions_matrix, user_features_matrix, item_features_matrix):
 
@@ -33,7 +35,6 @@ def generate_sparse_input(interactions_matrix, user_features_matrix, item_featur
 
 def generate_dummy_data(num_users=15000, num_items=30000, interaction_density=.00045, pos_int_ratio=.5):
 
-
     n_user_features = int(num_users * 1.2)
     n_user_tags = num_users * 3
     n_item_features = int(num_items * 1.2)
@@ -62,3 +63,22 @@ def generate_dummy_data(num_users=15000, num_items=30000, interaction_density=.0
         interactions[random.randrange(num_users), random.randrange(num_items)] = -1
 
     return interactions, user_features, item_features
+
+
+def generate_movielens_data(min_positive_rating=4.0):
+
+    data = fetch_movielens(indicator_features=True, genre_features=True)
+
+    train_interactions = data['train']
+    test_interactions = data['test']
+    item_features = data['item_features']
+
+    train_interactions.data = np.array([1 if value > min_positive_rating else -1
+                                        for value in train_interactions.data])
+    test_interactions.data = np.array([1 if value > min_positive_rating else -1
+                                       for value in test_interactions.data])
+
+    num_users = train_interactions.shape[0]
+    user_features = sp.identity(num_users)
+
+    return train_interactions, test_interactions, user_features, item_features
