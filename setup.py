5c152deaa19a8ca6385e6397bc089eda74073d16
jfkirk <james.f.kirk@gmail.com>
2017-10-30 12:58:49 -0400

    MAINT: Adds long description to setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
-from distutils.core import setup
+from setuptools import setup
+
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
   name='tensorrec',
   packages=['tensorrec'],
   version='0.1',
   description='A TensorFlow recommendation algorithm and framework in Python.',
+  long_description=long_description,
   author='James Kirk',
   author_email='james.f.kirk@gmail.com',
   url='https://github.com/jfkirk/tensorrec',
-  download_url='https://github.com/jfkirk/tensorrec/archive/0.1.tar.gz',
   keywords=['machine-learning', 'tensorflow', 'recommendation-system', 'python'],
   classifiers=[],
 )
