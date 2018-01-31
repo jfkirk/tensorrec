dfb3c3bb95c7bd606be3a8b80d5b5a92ce49af21
jfkirk <james.f.kirk@gmail.com>
2018-01-30 21:49:56 -0500

    MAINT: Fixes setup for py2

--- setup.py
+++ setup.py
@@ -4,16 +4,11 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
   name='tensorrec',
   packages=['tensorrec'],
-  version='0.12',
+  version='0.12.1',
   description='A TensorFlow recommendation algorithm and framework in Python.',
-  long_description=long_description,
   author='James Kirk',
   author_email='james.f.kirk@gmail.com',
   url='https://github.com/jfkirk/tensorrec',
