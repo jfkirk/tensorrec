0c978afd4f2c35124b383975dc6f0d6576df8c55
jfkirk <james.f.kirk@gmail.com>
2018-02-23 22:29:02 -0500

    MAINT: Sets version to 0.13

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = path.abspath(path.dirname(__file__))
 setup(
   name='tensorrec',
   packages=['tensorrec'],
-  version='0.12.2',
+  version='0.13',
   description='A TensorFlow recommendation algorithm and framework in Python.',
   author='James Kirk',
   author_email='james.f.kirk@gmail.com',
