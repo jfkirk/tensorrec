a5a8772ad0b980704cbb43db507682c247384c1f
jfkirk <james.f.kirk@gmail.com>
2018-02-25 21:38:25 -0500

    DEV: Ups version to 0.13.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = path.abspath(path.dirname(__file__))
 setup(
   name='tensorrec',
   packages=['tensorrec'],
-  version='0.13.2',
+  version='0.13.3',
   description='A TensorFlow recommendation algorithm and framework in Python.',
   author='James Kirk',
   author_email='james.f.kirk@gmail.com',
