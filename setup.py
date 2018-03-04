ae0a03703a6faacc838e7a80d071b81f7f02c0c0
jfkirk <james.f.kirk@gmail.com>
2018-03-04 11:43:19 -0500

    DEV: Ups version to 0.2.0.1

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ here = path.abspath(path.dirname(__file__))
 setup(
   name='tensorrec',
   packages=['tensorrec'],
-  version='0.2',
+  version='0.2.0.1',
   description='A TensorFlow recommendation algorithm and framework in Python.',
   author='James Kirk',
   author_email='james.f.kirk@gmail.com',
   url='https://github.com/jfkirk/tensorrec',
-  keywords=['machine-learning', 'tensorflow', 'recommendation-system', 'python'],
+  keywords=['machine-learning', 'tensorflow', 'recommendation-system', 'python', 'recommender-system'],
   classifiers=[],
 )
