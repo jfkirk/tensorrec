BUG: Fixes NDCG calculation by removing bad index

tensorrec/eval.py
@@ -129,7 +129,7 @@ def ndcg_at_k(model, test_interactions, k=10,
                                                                  k)
 
     dcg = np.asarray(_dcg(relevance, k_mask, ror_at_k, ranks_of_relevant))[0]
-    idcg = np.apply_along_axis(_idcg, 1, relevance.A)[0]
+    idcg = np.apply_along_axis(_idcg, 1, relevance.A)
 
     ndcg = dcg/idcg
 

test/test_eval.py
@@ -66,26 +66,32 @@ class EvalTestCase(TestCase):
 
     def test_ndcg_at_k(self):
 
-        ndcg10 = np.nanmean(ndcg_at_k(model=self.model,
-                                      test_interactions=self.interactions,
-                                      k=10,
-                                      user_features=self.user_features,
-                                      item_features=self.item_features,
-                                      preserve_rows=True))
+        ndcg10 = np.mean(ndcg_at_k(
+            model=self.model,
+            test_interactions=self.interactions,
+            k=10,
+            user_features=self.user_features,
+            item_features=self.item_features,
+            preserve_rows=False
+        ))
 
-        ndcg20 = np.nanmean(ndcg_at_k(model=self.model,
-                                      test_interactions=self.interactions,
-                                      k=20,
-                                      user_features=self.user_features,
-                                      item_features=self.item_features,
-                                      preserve_rows=True))
+        ndcg20 = np.mean(ndcg_at_k(
+            model=self.model,
+            test_interactions=self.interactions,
+            k=20,
+            user_features=self.user_features,
+            item_features=self.item_features,
+            preserve_rows=False
+        ))
 
-        ndcg5 = np.nanmean(ndcg_at_k(model=self.model,
-                                     test_interactions=self.interactions,
-                                     k=5,
-                                     user_features=self.user_features,
-                                     item_features=self.item_features,
-                                     preserve_rows=True))
+        ndcg5 = np.mean(ndcg_at_k(
+            model=self.model,
+            test_interactions=self.interactions,
+            k=5,
+            user_features=self.user_features,
+            item_features=self.item_features,
+            preserve_rows=False
+        ))
 
         self.assertIsNotNone(ndcg10)
         self.assertGreaterEqual(ndcg10, ndcg5)