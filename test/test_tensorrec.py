91d0e7b0296cc4ce34fd3e18fe282b1ca4b5095a
Logan Moore <lmooore@gmail.com>
2018-03-13 01:29:38 -0400

    Remove movielens requirement for test cases

--- test/test_tensorrec.py
+++ test/test_tensorrec.py
@@ -8,7 +8,6 @@ import tensorflow as tf
 from tensorrec import TensorRec
 from tensorrec.util import generate_dummy_data_with_indicator, generate_dummy_data
 from tensorrec.session_management import set_session
-from test.datasets import get_movielens_100k
 
 
 class TensorRecTestCase(TestCase):
@@ -133,21 +132,20 @@ class TensorRecMovielensPrediction(TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.movielens_100k = get_movielens_100k()
-        train_interactions, test_interactions, cls.user_features, cls.item_features, _ = cls.movielens_100k
-        cls.model = TensorRec()
-        cls.model.fit(
-            interactions=train_interactions,
-            user_features=cls.user_features,
-            item_features=cls.item_features,
-            epochs=5)
+        cls.interactions, cls.user_features, cls.item_features = generate_dummy_data(
+            num_users=15, num_items=30, interaction_density=.5, num_user_features=200, num_item_features=200,
+            n_features_per_user=20, n_features_per_item=20, pos_int_ratio=.5
+        )
+
+        cls.standard_model = TensorRec(n_components=10)
+        cls.standard_model.fit(cls.interactions, cls.user_features, cls.item_features, epochs=10)
 
     def test_predict_user_bias(self):
-        user_bias = self.model.predict_user_bias(self.user_features)
+        user_bias = self.standard_model.predict_user_bias(self.user_features)
         self.assertTrue(any(user_bias))  # Make sure it isn't all 0s
 
     def test_predict_item_bias(self):
-        item_bias = self.model.predict_item_bias(self.item_features)
+        item_bias = self.standard_model.predict_item_bias(self.item_features)
         self.assertTrue(any(item_bias))  # Make sure it isn't all 0s
 
 
