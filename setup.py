6c2ed87f918d8cbba7738349528191cd05c40eee
TariqAHassan <tariqaahassan@gmail.com>
2018-07-14 00:05:47 -0700

    Add `install_requires`

--- setup.py
+++ setup.py
@@ -5,13 +5,17 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 setup(
-  name='tensorrec',
-  packages=['tensorrec'],
-  version='0.25.5',
-  description='A TensorFlow recommendation algorithm and framework in Python.',
-  author='James Kirk',
-  author_email='james.f.kirk@gmail.com',
-  url='https://github.com/jfkirk/tensorrec',
-  keywords=['machine-learning', 'tensorflow', 'recommendation-system', 'python', 'recommender-system'],
-  classifiers=[],
+    name='tensorrec',
+    packages=['tensorrec'],
+    version='0.25.5',
+    description='A TensorFlow recommendation algorithm and framework in Python.',
+    author='James Kirk',
+    author_email='james.f.kirk@gmail.com',
+    url='https://github.com/jfkirk/tensorrec',
+    install_requires=[
+        'requests', 'numpy', 'scipy',
+        'sklearn', 'lightfm', 'tensorflow'
+    ],
+    keywords=['machine-learning', 'tensorflow', 'recommendation-system', 'python', 'recommender-system'],
+    classifiers=[]
 )
