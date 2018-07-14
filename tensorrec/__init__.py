1a8479b1bb67835a940570155fa44cebcd7ef1f2
TariqAHassan <tariqaahassan@gmail.com>
2018-07-13 23:54:56 -0700

    Bump version

--- tensorrec/__init__.py
+++ tensorrec/__init__.py
@@ -8,7 +8,7 @@ from . import prediction_graphs
 from . import session_management
 from . import util
 
-__version__ = '0.25.4'
+__version__ = '0.25.5'
 
 __all__ = [
     TensorRec, eval, util, loss_graphs,
