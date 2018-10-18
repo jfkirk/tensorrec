b026ecf3f436e896d47530b1404662aeeeaa498a
jfkirk <james.f.kirk@gmail.com>
2018-10-18 10:44:47 -0400

    DEV: Updates version to 0.25.7

--- setup.py
+++ setup.py
@@ -2,18 +2,19 @@ from setuptools import setup
 
 from os import path
 
+
 def parse_requirements(filename):
     """ load requirements from a pip requirements file """
     lineiter = (line.strip() for line in open(filename))
     return [line for line in lineiter if line and not line.startswith("#")]
 
 here = path.abspath(path.dirname(__file__))
-install_reqs = parse_requirements(path.join(here, 'requirements.txt'))
+install_reqs = parse_requirements('requirements.txt')
 
 setup(
   name='tensorrec',
   packages=['tensorrec'],
-  version='0.25.6',
+  version='0.25.7',
   description='A TensorFlow recommendation algorithm and framework in Python.',
   author='James Kirk',
   author_email='james.f.kirk@gmail.com',
