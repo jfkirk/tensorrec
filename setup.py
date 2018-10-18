b98542c596392a950f9a8188b0235e17e9fa2c42
jfkirk <james.f.kirk@gmail.com>
2018-10-18 10:33:07 -0400

    DEV: Updates version to 0.25.6

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
-from pip.req import parse_requirements
-from pip.download import PipSession
 from setuptools import setup
 
 from os import path
 
-here = path.abspath(path.dirname(__file__))
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
 
-install_reqs = parse_requirements(path.join(here, 'requirements.txt'), session=PipSession())
-reqs = [str(ir.req) for ir in install_reqs]
+here = path.abspath(path.dirname(__file__))
+install_reqs = parse_requirements(path.join(here, 'requirements.txt'))
 
 setup(
   name='tensorrec',
   packages=['tensorrec'],
-  version='0.25.5',
+  version='0.25.6',
   description='A TensorFlow recommendation algorithm and framework in Python.',
   author='James Kirk',
   author_email='james.f.kirk@gmail.com',
   url='https://github.com/jfkirk/tensorrec',
   keywords=['machine-learning', 'tensorflow', 'recommendation-system', 'python', 'recommender-system'],
   classifiers=[],
-  install_requires=reqs,
+  install_requires=install_reqs,
 )
